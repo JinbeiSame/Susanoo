4a8c0a5d69afd2ae3a78f4f8977471e6fd71ead7
Masaki Samejima <samejima@ist.osaka-u.ac.jp>
2015-03-16 17:19:12 +0100

    Update model & remove setting

--- Susanoo/app/models.py
+++ Susanoo/app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.template import Template
+import sys
+import urllib
+import json
+import instance.
 
 # Create your models here.
+
+class Weather(models.Model):
+    print "aa"
